dc826a4c5b0cf45ad65657a1f3700c76e2729d33
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-04-02 21:24:33 +0200

    refactor: Setup dotenv file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:7777',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const targetHost = env.API_URL || 'http://localhost:77777';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: targetHost,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
-  }
-})
+  };
+});
