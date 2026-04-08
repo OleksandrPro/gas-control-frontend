e99d2e3c14dd6b8e5560a9914298b8239a8164c9
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-04-08 11:36:11 +0200

    fix: Rename environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
-  const targetHost = env.API_URL || 'http://localhost:77777';
+  const targetHost = env.VITE_API_URL || 'http://localhost:77777';
 
   return {
     plugins: [react()],
